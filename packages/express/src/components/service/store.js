b40097b40f725120f8b16014f07b8ac0b5c42928
Nimrod León <nleonc14@gmail.com>
2021-05-17 21:25:43 -0500

    RD4-5 funciones crud pagos

---
+++ packages/express/src/components/service/store.js
@@ -0,0 +1,30 @@
+import {Service} from './model'
+
+// Listar servicios.
+export async function getServices(clientId) {
+  return Service.find({client: clientId})
+    .populate('servicePlan')
+}
+
+// devolver servicio por id.
+export async function getService(id) {
+  return Service.findById(id)
+}
+
+// registrar servicio.
+export async function createService(data, userId) {
+  let _service = new Service(data)
+  _service.user = userId
+  await _service.save()
+  return _service
+}
+
+// actualizar servicio.
+export async function updateService(id, data) {
+  return Service.findByIdAndUpdate(id, data, {new: true})
+}
+
+// borrar servicio.
+export async function deleteService(id) {
+  return Service.findByIdAndDelete(id)
+}
