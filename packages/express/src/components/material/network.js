7922bc1b5d89200894149877785521e1c10d93a3
Nimrod León <nleonc14@gmail.com>
2021-05-20 11:48:48 -0500

    RD4-11 capa de red material

---
+++ packages/express/src/components/material/network.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import * as controller from './controller'
+import verifyToken from '../auth/verifyToken'
+
+const router = express.Router()
+
+// Listar materiales.
+router.get('/', verifyToken, async (req, res) => {
+  let query = req.query.search || ''
+  controller.getMaterials(query).then(result => {
+    res.json(result)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// obtener material por id.
+router.get('/:id', verifyToken, async (req, res) => {
+  controller.getMaterial(req.params.id).then(result => {
+    res.json(result)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// cantidad de material en tarea.
+router.get('/:id/count/material', verifyToken, async (req, res) => {
+  res.json(0)
+})
+
+// registrar material.
+router.post('/', verifyToken, async (req, res) => {
+  controller.createMaterial(req.body).then(result => {
+    res.json(result)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// actualizar material.
+router.patch('/:id', verifyToken, async (req, res) => {
+  controller.updateMaterial(req.params.id, req.body).then(result => {
+    res.json(result)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// borrar material.
+router.delete('/:id', verifyToken, async (req, res) => {
+  controller.deleteMaterial(req.params.id).then(result => {
+    res.json(result)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+export default router
