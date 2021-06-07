8ccf480926c28550992f02d22bfcb6c843ca34cd
Nimrod León <nleonc14@gmail.com>
2021-06-07 17:46:52 -0500

    refactorización Unauthorized login

--- packages/angular/src/app/auth.guard.ts
+++ packages/angular/src/app/auth.guard.ts
@@ -15,7 +15,9 @@ export class AuthGuard implements CanActivate {
     if (this.authService.loggedIn()) {
       return true;
     }
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login']).then(() => {
+      console.info('[Unauthorized]');
+    });
     return false;
   }
 
