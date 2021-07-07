7c31b563878d515eef0db1471a3c99cd2c05fc1a
Nimrod León <nleonc14@gmail.com>
2021-07-07 11:09:42 -0500

    definición modelo mikrotik

--- packages/express/src/components/mikrotik/model.js
+++ packages/express/src/components/mikrotik/model.js
@@ -1,7 +1,37 @@
 import {model, Schema} from 'mongoose'
 
 // Schema Mikrotik.
-const mikrotikSchema = new Schema({})
+const mikrotikSchema = new Schema({
+  name: String,
+  host: String,
+  port: Number,
+  userName: String,
+  password: String,
+  applicationId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Application'
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+})
 
 // exportar modelo mikrotik.
 export const Mikrotik = model('Mikrotik', mikrotikSchema)
+
+// Schema ethernet mikrotik.
+const ethernetSchema = new Schema({
+  name: String,
+  mikrotikId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Mikrotik'
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+})
+
+// exportar modelo ethernet.
+export const Ethernet = model('Ethernet', ethernetSchema)
