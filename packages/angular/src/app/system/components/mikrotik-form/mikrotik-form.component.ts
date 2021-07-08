ee465c71e83c9492435bdf38e21b8891db5c15a4
Nimrod León <nleonc14@gmail.com>
2021-07-07 19:22:13 -0500

    avance formulario mikrotik

--- packages/angular/src/app/system/components/mikrotik-form/mikrotik-form.component.ts
+++ packages/angular/src/app/system/components/mikrotik-form/mikrotik-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Application} from '../../interfaces';
+import {ApplicationService, EthernetService, InfoService, MikrotikService} from '../../services';
 
 @Component({
   selector: 'app-mikrotik-form',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mikrotik-form.component.scss']
 })
 export class MikrotikFormComponent implements OnInit {
+  @Input()
+  editMode: boolean;
+  @Input()
+  currentMikrotik;
+  title: string;
+  applicationList: Array<Application>;
 
-  constructor() { }
+  constructor(
+    private mikrotikService: MikrotikService,
+    private ethernetService: EthernetService,
+    private applicationService: ApplicationService) {
+  }
 
   ngOnInit(): void {
+    this.getApplicationList();
+    let myModal = document.querySelector('#mikrotik-form');
+    myModal.addEventListener('show.bs.modal', () => {
+      if (this.editMode === false) {
+        // this.currentMikrotik = this.mikrotikService.defaultValues();
+        this.title = 'Agregar Mikrotik';
+      } else {
+        this.title = 'Editar Mikrotik';
+      }
+    });
+  }
+
+  // Lista de aplicaciones.
+  getApplicationList(): void {
+    this.applicationService.getApplications()
+      .subscribe(result => this.applicationList = result);
   }
 
 }
--- packages/angular/src/app/system/pages/mikrotik-list/mikrotik-list.component.ts
+++ packages/angular/src/app/system/pages/mikrotik-list/mikrotik-list.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {MikrotikService} from '../../services';
+import {Mikrotik} from '../../interfaces';
 
 declare var jQuery: any;
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-mikrotik-list',
@@ -8,16 +11,47 @@ declare var jQuery: any;
   styleUrls: ['./mikrotik-list.component.scss']
 })
 export class MikrotikListComponent implements OnInit {
+  mikrotikModal: any;
+  currentMikrotik: Mikrotik;
+  mikrotikList: Array<Mikrotik>;
+  editMode: boolean = false;
 
-  constructor() {
+  constructor(
+    private mikrotikService: MikrotikService) {
+    // establecer valores por defecto.
+    this.currentMikrotik = this.mikrotikService.defaultValues();
   }
 
   ngOnInit(): void {
+    // Establecer modal mikrotik form.
+    this.mikrotikModal = new bootstrap.Modal(
+      document.querySelector('#mikrotik-form'));
+    this.getMikrotikList();
+  }
+
+  // Lista de mikrotik.
+  private getMikrotikList(): void {
+    this.mikrotikService.getMikrotikList()
+      .subscribe(result => {
+        this.mikrotikList = result;
+        // cargar primer elemento de la lista.
+        if (result.length > 0) {
+          this.loadCurrentMikrotik(result[0]._id);
+        }
+      });
+  }
+
+  // cargar mikrotik actual.
+  loadCurrentMikrotik(id: string): void {
+    this.mikrotikService.getMikrotikById(id).subscribe(result => {
+      this.currentMikrotik = result;
+    });
   }
 
   // Agregar Mikrotik.
   addMikrotikClick(): void {
-    jQuery('#mikrotik-form').modal('show');
+    this.editMode = false;
+    this.mikrotikModal.show();
   }
 
 }
