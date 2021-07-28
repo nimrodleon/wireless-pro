e3711811cdf47fc3183751bf7996b67c05bbfcc2
Nimrod León <nleonc14@gmail.com>
2021-07-27 19:14:55 -0500

    arp controller completado

--- packages/express/src/components/worker/network.js
+++ packages/express/src/components/worker/network.js
@@ -58,5 +58,67 @@ function getSimpleQueueByIpAddress(req, res = response) {
 
 // ====================================================================================================
 
+// http://<HOST>/api/bitWorker/:id/getArpList
+router.get('/:id/getArpList', [verifyToken], getArpList)
+
+// Lista arp.
+function getArpList(req, res = response) {
+  WorkerController.getArpList(req.params.id).then(({data}) => {
+    res.json(data)
+  })
+}
+
+// http://<HOST>/api/bitWorker/:id/getArpListById/:arpId
+router.get('/:id/getArpListById/:arpId', [verifyToken], getArpListById)
+
+// Obtener arp por Id.
+function getArpListById(req, res = response) {
+  let {id, arpId} = req.params
+  WorkerController.getArpListById(arpId, id).then(({data}) => {
+    res.json(data)
+  })
+}
+
+// http://<HOST>/api/bitWorker/:id/getArpListByDisabled/:value
+router.get('/:id/getArpListByDisabled/:value', [verifyToken], getArpListByDisabled)
+
+// cargar lista arp por campo [disabled]
+function getArpListByDisabled(req, res = response) {
+  let {id, value} = req.params
+  WorkerController.getArpListByDisabled(value, id).then(({data}) => {
+    res.json(data)
+  })
+}
+
+// http://<HOST>/api/bitWorker/createArpList
+router.post('/createArpList', [verifyToken], createArpList)
+
+// registrar arp item.
+function createArpList(req, res = response) {
+  WorkerController.createArpList(req.body).then(({data}) => {
+    res.json(data)
+  })
+}
+
+// http://<HOST>/api/bitWorker/updateArpList/:id
+router.put('/updateArpList/:id', [verifyToken], updateArpList)
+
+// actualizar arp item.
+function updateArpList(req, res = response) {
+  WorkerController.updateArpList(req.params.id, req.body).then(({data}) => {
+    res.json(data)
+  })
+}
+
+// http://<HOST>/api/bitWorker/:id/deleteArpList/:arpId
+router.delete('/:id/deleteArpList/:arpId', [verifyToken], deleteArpList)
+
+// borrar arp item.
+function deleteArpList(req, res = response) {
+  let {id, arpId} = req.params
+  WorkerController.deleteArpList(arpId, id).then(({data}) => {
+    res.json(data)
+  })
+}
 
 export const bitWorkerRouter = router
