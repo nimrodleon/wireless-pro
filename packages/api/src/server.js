555f8d0e2545d514d81e574ebcd2721022d12c86
Nimrod <nleonc14@gmail.com>
2023-05-27 19:08:44 -0500

    backend packages updated

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -12,13 +12,13 @@ const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 // Simple Usage (Enable All CORS Requests)
 app.use(cors())
 const dbname = process.env.DB_NAME || 'rd4-server'
 mongoose.connect(`mongodb://127.0.0.1:27017/${dbname}`, {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+  useNewUrlParser: true, useUnifiedTopology: true,
 }).then(async () => {
   router(app)
   console.log(dbname)
