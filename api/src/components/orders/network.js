6a325f007fc1a22d0aa162e2252e8b7f85462088
Nimrod León <nleonc14@gmail.com>
2023-11-07 19:29:25 -0500

    refactoring ordenes de servicio

--- api/src/components/orders/network.js
+++ api/src/components/orders/network.js
@@ -1,26 +1,26 @@
-import express, {response} from 'express'
-import {checkRolAdmin, verifyToken} from '../middlewares'
-import {OrderController} from './controller'
+import express, {response} from "express"
+import {checkRolAdmin, verifyToken} from "../middlewares"
+import {OrderController} from "./controller"
 
 const router = express.Router()
 
 // http://<HOST>/api/work_orders
-router.get('/', [verifyToken], getOrderList)
+router.get("/", [verifyToken], getOrderList)
 
 // Lista de ordenes de trabajo.
 function getOrderList(req, res = response) {
-  let {search = ''} = req.query
-  OrderController.getOrderList(search).then(result => {
+  let {search = ""} = req.query
+  OrderController.getOrderList(search.toUpperCase()).then(result => {
     res.json(result)
   })
 }
 
 // http://<HOST>/api/work_orders/:year/:month/report
-router.get('/:year/:month/report', [verifyToken], getOrderListByYearMonth)
+router.get("/:year/:month/report", [verifyToken], getOrderListByYearMonth)
 
 // Lista de ordenes filtrado por mes y año.
 function getOrderListByYearMonth(req, res = response) {
-  let {search = ''} = req.query
+  let {search = ""} = req.query
   let {year, month} = req.params
   OrderController.getOrderListByYearMonth(year, month, search).then(result => {
     res.json(result)
@@ -28,7 +28,7 @@ function getOrderListByYearMonth(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/:id
-router.get('/:id', [verifyToken], getOrderById)
+router.get("/:id", [verifyToken], getOrderById)
 
 // Obtener orden de instalación por id.
 function getOrderById(req, res = response) {
@@ -38,7 +38,7 @@ function getOrderById(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/:id/client
-router.get('/:id/client', [verifyToken], getOrderByClientId)
+router.get("/:id/client", [verifyToken], getOrderByClientId)
 
 // Obtener orden de instalación por id del cliente.
 function getOrderByClientId(req, res = response) {
@@ -48,7 +48,7 @@ function getOrderByClientId(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders
-router.post('/', [verifyToken], addOrder)
+router.post("/", [verifyToken], addOrder)
 
 // Registrar orden de instalación.
 function addOrder(req, res = response) {
@@ -58,7 +58,7 @@ function addOrder(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/:id
-router.patch('/:id', [verifyToken], updateOrder)
+router.patch("/:id", [verifyToken], updateOrder)
 
 // actualizar orden de instalación.
 function updateOrder(req, res = response) {
@@ -68,7 +68,7 @@ function updateOrder(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/:id
-router.delete('/:id', [verifyToken, checkRolAdmin], deleteOrder)
+router.delete("/:id", [verifyToken, checkRolAdmin], deleteOrder)
 
 // Borrar orden de instalación.
 function deleteOrder(req, res = response) {
@@ -80,7 +80,7 @@ function deleteOrder(req, res = response) {
 // ====================================================================================================
 
 // http://<HOST>/api/work_orders/:id/material
-router.get('/:id/material', [verifyToken], getMaterials)
+router.get("/:id/material", [verifyToken], getMaterials)
 
 // Lista de materiales.
 function getMaterials(req, res = response) {
@@ -90,7 +90,7 @@ function getMaterials(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/show/material/:id
-router.get('/show/material/:id', [verifyToken], getMaterial)
+router.get("/show/material/:id", [verifyToken], getMaterial)
 
 // Obtener material por id.
 function getMaterial(req, res = response) {
@@ -100,7 +100,7 @@ function getMaterial(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/add/material
-router.post('/add/material', [verifyToken], addMaterial)
+router.post("/add/material", [verifyToken], addMaterial)
 
 // registrar material.
 function addMaterial(req, res = response) {
@@ -110,7 +110,7 @@ function addMaterial(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/update/material/:id
-router.patch('/update/material/:id', [verifyToken], updateMaterial)
+router.patch("/update/material/:id", [verifyToken], updateMaterial)
 
 // actualizar material.
 function updateMaterial(req, res = response) {
@@ -120,7 +120,7 @@ function updateMaterial(req, res = response) {
 }
 
 // http://<HOST>/api/work_orders/delete/material/:id
-router.delete('/delete/material/:id', [verifyToken], deleteMaterial)
+router.delete("/delete/material/:id", [verifyToken], deleteMaterial)
 
 // borrar material.
 function deleteMaterial(req, res = response) {
